29fb47d601f71423da44783023ccacbe332fc9b3
Erzh <reyzerlg@inbox.ru>
2023-04-21 00:04:25 +0600

    task #3

--- Seminars/Seminar 1/Program.cs
+++ Seminars/Seminar 1/Program.cs
@@ -4,23 +4,36 @@
 
 // for comment use (ctrl + /) надо выделить
 
-//task 1. Запросить число вывести его квадрат
+//Task #1. Запросить число вывести его квадрат
 
 // Console.WriteLine("input number:");
 // int number = Convert.ToInt32(Console.ReadLine());
 // Console.WriteLine($"square of number {number} = {number*number}");
 
-//task 2. Является ли 1 число квадратом второго
+//Task #2. Является ли 1 число квадратом второго
 
-Console.WriteLine("input first number:");
-int number1 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("input second number:");
-int number2 = Convert.ToInt32(Console.ReadLine());
-if(number1 == number2*number2)
-{
-     Console.WriteLine($"{number1} is square of {number2}");
-}
-else
-{
-    Console.WriteLine($"{number1} is not square of {number2}");
-}
+// Console.WriteLine("input first number:");
+// int number1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("input second number:");
+// int number2 = Convert.ToInt32(Console.ReadLine());
+// if(number1 == number2*number2)
+// {
+//      Console.WriteLine($"{number1} is square of {number2}");
+// }
+// else
+// {
+//     Console.WriteLine($"{number1} is not square of {number2}");
+// }
+
+//Task #3. Напишите программу которая принимает на вход число N, а на выходе получаем последовательность от -N до N
+
+// Console.WriteLine("input number:");
+// int n = Convert.ToInt32(Console.ReadLine());
+// if(n<0) Console.WriteLine("invalid input N");
+// int count = n*(-1);
+// while(count <= n)
+// {
+//     Console.Write(count + ", ");
+//     count++;
+// }
+
