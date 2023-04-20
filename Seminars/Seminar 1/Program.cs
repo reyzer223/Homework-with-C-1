17e985799bc17d4a976903ebf395569b09cf96ed
Erzh <reyzerlg@inbox.ru>
2023-04-20 23:35:55 +0600

    task #1

--- Seminars/Seminar 1/Program.cs
+++ Seminars/Seminar 1/Program.cs
@@ -1,3 +1,11 @@
-﻿Console.WriteLine("input number:");
+﻿// Console.WriteLine("input number:");
+// int number = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine($"You input {{{number}}} number");
+
+// for comment use (ctrl + /) надо выделить
+
+//task 1 Запросить число вывести его квадрат
+
+Console.WriteLine("input number:");
 int number = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine($"You input {{{number}}} number");
+Console.WriteLine($"square of number {number} = {number*number}");
