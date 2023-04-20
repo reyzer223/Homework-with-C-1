4888c17ccd0b8215c5efc4b6b6fa5d4ecb95f41a
Erzh <reyzerlg@inbox.ru>
2023-04-20 23:45:04 +0600

    task #2

--- Seminars/Seminar 1/Program.cs
+++ Seminars/Seminar 1/Program.cs
@@ -4,8 +4,23 @@
 
 // for comment use (ctrl + /) надо выделить
 
-//task 1 Запросить число вывести его квадрат
+//task 1. Запросить число вывести его квадрат
 
-Console.WriteLine("input number:");
-int number = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine($"square of number {number} = {number*number}");
+// Console.WriteLine("input number:");
+// int number = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine($"square of number {number} = {number*number}");
+
+//task 2. Является ли 1 число квадратом второго
+
+Console.WriteLine("input first number:");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("input second number:");
+int number2 = Convert.ToInt32(Console.ReadLine());
+if(number1 == number2*number2)
+{
+     Console.WriteLine($"{number1} is square of {number2}");
+}
+else
+{
+    Console.WriteLine($"{number1} is not square of {number2}");
+}
