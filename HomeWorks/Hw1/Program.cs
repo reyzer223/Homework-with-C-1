ed4858c6cc238bd5a6170b0310162330717b11fd
Erzh <reyzerlg@inbox.ru>
2023-04-23 21:33:15 +0600

    task #1,#2,#3 finish

---
+++ HomeWorks/Hw1/Program.cs
@@ -0,0 +1,53 @@
+﻿// Task #1.
+
+// Console.WriteLine("Input first number: ");
+// int number1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Input second number: ");
+// int number2 = Convert.ToInt32(Console.ReadLine());
+// if (number1 > number2)
+//     {
+//         Console.WriteLine($"{number1} is bigger of {number2}");
+//     }
+// else
+//     {
+//         Console.WriteLine($"{number2} is bigger of {number1}");
+//     }
+
+//-----------------------------------------------------------------------------//
+
+// Task #2.
+
+// Console.WriteLine("Input first number: ");
+// int number1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Input second number: ");
+// int number2 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Input third number: ");
+// int number3 = Convert.ToInt32(Console.ReadLine());
+
+// if (number1 > number2 && number1 > number3)
+// {
+//     Console.WriteLine($"Max number is {number1}");
+// }
+// else if (number2 > number1 && number2 > number3)
+// {
+//      Console.WriteLine($"Max number is {number2}");
+// }
+// if (number3 > number2 && number3 > number1)
+// {
+//     Console.WriteLine($"Max number is {number3}");
+// }
+
+//-----------------------------------------------------------------------------//
+
+
+// Task #3.
+
+// Console.WriteLine("input number: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// int ed = number % 50;
+// if (number == number / ed)
+//     {
+//         Console.Write($"{number} is even number");
+//     }
+//     Console.Write($"{number} is not even number");
+
