494c9d885b2ea24a163e4c4b890feb5b0d202c61
Erzh <reyzerlg@inbox.ru>
2023-05-18 23:16:02 +0600

    Lesson 4 method1

---
+++ Seminars/Seminar 4/Program.cs
@@ -0,0 +1,15 @@
+﻿int DigitCount (int num)
+{
+    int count = 0;
+    while (num  0)
+    {
+        count++;
+        num/=10;
+    }
+    return count;
+}
+
+Console.WriteLine("Input Number");
+int num = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Number of digits in{num} -> {DigitCount(num)}");
